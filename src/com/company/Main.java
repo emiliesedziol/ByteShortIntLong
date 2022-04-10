63ef127a92dcfea09363743d01ed6142e57621e0
emiliesedziol <sedziole@gmail.com>
2022-04-10 16:52:22 -0400

    int min and max

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int intValue = 1000;
+
+        int intMinIntValue = Integer.MIN_VALUE;
+        int intMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum value = " + intMinIntValue);
+        System.out.println("Integer maximum value = " + intMaxIntValue);
+        /*
+        Integer Minimum value = -2147483648
+        Integer maximum value = 2147483647
+         */
+
+        System.out.println("Bust MAX value = " + (intMaxIntValue + 1));
+        // Bust MAX value = -2147483648
     }
 }
