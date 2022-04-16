8659cc185c45d5e66a9440b7dd9f3abe131832f9
emiliesedziol <sedziole@gmail.com>
2022-04-16 16:06:55 -0400

    primitive

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,5 +17,64 @@ public class Main {
 
         System.out.println("Bust MAX value = " + (intMaxIntValue + 1));
         // Bust MAX value = -2147483648
+
+        byte byteMinByteValue = Byte.MIN_VALUE;
+        byte byteMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum value = " + byteMinByteValue);
+        System.out.println("Byte maximum value = " + byteMaxByteValue);
+
+        System.out.println("Bust Minimum value = " + (byteMinByteValue-1));
+        System.out.println("Bust maximum value = " + (byteMaxByteValue+1));
+
+        /*
+         Byte Minimum value = -128
+        Byte maximum value = 127
+        Bust Minimum value = -129
+        Bust maximum value = 128
+        */
+
+        short shortMinByteValue = Short.MIN_VALUE;
+        short shortMaxByteValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum value = " + shortMinByteValue);
+        System.out.println("Short maximum value = " + shortMaxByteValue);
+
+        System.out.println("Bust Minimum value = " + (shortMinByteValue-1));
+        System.out.println("Bust maximum value = " + (shortMaxByteValue+1));
+
+        /*
+        Short Minimum value = -32768
+        Short maximum value = 32767
+        Bust Minimum value = -32769
+        Bust maximum value = 32768
+         */
+
+        long longMinByteValue = Long.MIN_VALUE;
+        long longMaxByteValue = Long.MAX_VALUE;
+
+        System.out.println("long Minimum value = " + longMinByteValue);
+        System.out.println("long maximum value = " + longMaxByteValue);
+
+        System.out.println("Bust Minimum value = " + (longMinByteValue-1));
+        System.out.println("Bust maximum value = " + (longMaxByteValue+1));
+
+        long longValue = 100L;
+
+        System.out.println("Long 100L = "+ longValue);
+        long longBigLongLitteral = 2_137_432_324_478L;
+
+        System.out.println(longBigLongLitteral);
+        /*
+        long Minimum value = -9223372036854775808
+        long maximum value = 9223372036854775807
+        Bust Minimum value = 9223372036854775807
+        Bust maximum value = -9223372036854775808
+        Long 100L = 100
+        2137432324478
+         */
+
+
+
     }
 }
